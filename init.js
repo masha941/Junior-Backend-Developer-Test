0698ed5e67fc0471613c441824d11b388aa51bb4
Masa Lazic <masalazic56@gmail.com>
2026-01-29 21:17:48 +0100

    Make initialization script

---
+++ init.js
@@ -0,0 +1,15 @@
+/* global use, db, ObjectId, print */
+
+use grocery_store_db;
+
+Db.offices.insertOne({
+    id:,
+    name:,
+    parentId:
+})
+
+Db.stores.insertOne({
+    id:,
+    name:,
+    parentId:,
+})
